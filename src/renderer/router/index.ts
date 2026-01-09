137b4fcdbb3931ee9a690d3df27b14a48f22d5cd
Tu1231 <1799405509@qq.com>
2026-01-09 17:29:36 +0800

    fix:build router problem

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -1,10 +1,10 @@
-import { createWebHistory, createRouter } from 'vue-router';
+import { createWebHashHistory, createRouter } from 'vue-router';
 import type { App } from 'vue';
 
 import { routes } from './routes/index';
 
 const router = createRouter({
-   history: createWebHistory(),
+   history: createWebHashHistory(),
    routes,
 });
 
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -9,6 +9,7 @@ import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
+   base: './',
    plugins: [
       vue(),
       tailwindcss(),
